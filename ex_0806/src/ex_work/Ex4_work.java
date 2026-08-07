7f2e8f988f018665efdc19e365c15c4e38eb1e66
kms73289-cmyk <kms73289@gmail.com>
2026-08-07 17:07:49 +0900

    0806 수업내용

---
+++ ex_0806/src/ex_work/Ex4_work.java
@@ -0,0 +1,50 @@
+package ex_work;
+
+import java.util.Scanner;
+
+public class Ex4_work {
+    public static void main(String[] args) {
+
+        // 연산자는 스캐너의 .next()를 사용하자
+        // 수1 : 10
+        // 수2 : 20
+        // 연산자 : +
+        // 10 + 20 = 30
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("수1 : ");
+        int su1 = sc.nextInt();
+
+        System.out.print("수2 : ");
+        int su2 = sc.nextInt();
+
+        System.out.print("연산자 : ");
+        String op = sc.next();
+
+        switch (op) {
+
+            case "+":
+                System.out.println(su1 + " + " + su2 + " = " + (su1 + su2));
+                break;
+
+            case "-":
+                System.out.println(su1 + " - " + su2 + " = " + (su1 - su2));
+                break;
+
+            case "*":
+                System.out.println(su1 + " * " + su2 + " = " + (su1 * su2));
+                break;
+
+            case "/":
+                System.out.println(su1 + " / " + su2 + " = " + ((float) su1 / su2));
+                break;
+
+            default:
+                System.out.println("연산자 오류");
+                break;
+
+        }
+
+    }// main
+
+}
