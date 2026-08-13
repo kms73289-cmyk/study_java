864976d0ac0a43e3f48991e3c348d9aef612a05d
kms73289-cmyk <kms73289@gmail.com>
2026-08-13 17:47:43 +0900

    0813 수업내용

---
+++ ex_0813/src/ex_work/Ex01_work.java
@@ -0,0 +1,34 @@
+package ex_work;
+
+public class Ex01_work {
+    public static void main(String[] args) {
+        
+        //배열 arr에 담긴 모든 값의 합과 평균 구하기
+        //----------------
+        //총합 : 119
+        //평균 : 7.43
+
+        int[][] arr = { { 7, 8, 11, 6}, 
+                        { 20, 13, 9, 2, 5}, 
+                        { 7, 7, 10}, 
+                        { 2, 3, 4, 5} };
+
+        int total = 0;
+        float avg = 0;
+        int cnt = 0;
+
+        for( int i = 0; i < arr.length; i++){
+
+            for(int j = 0; j < arr[i].length; j++){
+
+                total += arr[i][j];
+                cnt++;
+
+            }
+        }System.out.println("총합 : " + total);
+        avg = (float)total / cnt;
+        System.out.printf("평균 %.2f",avg);
+        
+    }//main
+    
+}//class
---
+++ ex_0813/src/ex_work/Ex07_work.java
@@ -0,0 +1,27 @@
+package ex_work;
+
+import java.util.Scanner;
+
+public class Ex07_work {
+    public static void main(String[] args) {
+        
+        //홀수 : 3
+        // 08 01 06
+        // 03 05 07
+        // 04 09 02
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("홀수 : ");
+        int n = sc.nextInt();
+
+        int[][] num = new int[n][n];
+
+        for(int i = 1; i <= n; i++){
+
+        
+        }
+        
+
+    }//main
+    
+}//class
