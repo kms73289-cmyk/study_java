b02663261b1e83dd5b0447eeb61c5daa5c03d6fe
kms73289-cmyk <kms73289@gmail.com>
2026-08-14 16:35:08 +0900

    0813 수업내용

--- ex_0813/src/ex_work/Ex07_work.java
+++ ex_0813/src/ex_work/Ex07_work.java
@@ -11,17 +11,48 @@ public class Ex07_work {
         // 04 09 02
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("홀수 : ");
-        int n = sc.nextInt();
+        System.out.print("홀수 : ");
+        int size = sc.nextInt();
 
-        int[][] num = new int[n][n];
+        int[][] square = new int[size][size];
 
-        for(int i = 1; i <= n; i++){
+        int num = 1;
+        int y = 0; //y축 (행)
+        int x = 0; //x축 (열)
 
-        
+        x = size / 2;
+
+        while( num  <= size * size){
+
+            square[y][x] = num;
+
+            if(num % size == 0){
+                y++;
+            }else{
+
+                y--;
+                x++;
+            }
+
+            if( y < 0)
+                y = size - 1;
+
+            if(x >= size)
+                x=0;
+
+            num++;
+
         }
-        
 
+        //배열에 담긴 모든 값을 출력
+        for( int i = 0; i < size; i++){
+
+            for( int j = 0; j < size; j++){
+                System.out.printf("%02d ",square[i][j]);
+            }System.out.println();
+        }
+
+        
     }//main
     
 }//class
